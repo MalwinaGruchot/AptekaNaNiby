2804d84e9e5d32ce5507f2145e5f750918c232d3
MalwinaGruchot <malwinagruchot@wp.pl>
2023-09-01 00:43:17 +0200

    make logo

---
+++ src/components/LogoApp/LogoApp.jsx
@@ -0,0 +1,10 @@
+import "./LogoApp.scss";
+
+export default function LogoApp() {
+  return (
+    <div className="logo">
+      Apteka<span className="logo__txtFirst">Na</span>
+      <p className="logo__txtSecend">Niby</p>
+    </div>
+  );
+}
