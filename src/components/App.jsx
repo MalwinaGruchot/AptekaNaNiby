29b8f5de2d13b48b92b842eb656536bc47581012
MalwinaGruchot <malwinagruchot@wp.pl>
2023-09-01 22:14:29 +0200

    make app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,19 @@
 import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import RouteWrapper from "./RouteWrapper.jsx/RouteWrapper";
+import RouteWrapper from "./RouteWrapper/RouteWrapper";
+import Home from "./Home/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<RouteWrapper />}>
-          <Route path="/" element={<div>Home</div>} />
+          <Route path="/" element={<Home />} />
           <Route path="/promocje" element={<div>Promocje</div>} />
           <Route path="/pielęgnacja" element={<div>pielęgnacja</div>} />
           <Route path="/srodkiopatrunkowe" element={<div>opatrunkowe</div>} />
           <Route path="/witaminy" element={<div>witaminy</div>} />
-          <Route path="/sprzętmedyczny" element={<div>Sprzet medyczny</div>} />
+          <Route path="/sprzętmedyczny" element={<div>Sprzet</div>} />
         </Route>
       </Routes>
     </BrowserRouter>
