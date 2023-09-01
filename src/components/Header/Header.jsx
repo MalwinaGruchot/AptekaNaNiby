269df3904d69386f2c508a8b67e470c17aadd625
MalwinaGruchot <malwinagruchot@wp.pl>
2023-09-01 13:19:19 +0200

    make header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,28 +3,24 @@ import LogoApp from "../LogoApp/LogoApp";
 import HeaderWidget from "./HeaderWidget";
 import HeaderForm from "./HeaderForm";
 import HeaderMenu from "./HeaderMenu";
-import { Outlet } from "react-router-dom";
 
 export default function Header() {
   return (
-    <>
-      <header className="header">
-        <HeaderWidget />
-        <div className="header__serch">
-          <div className="container header__container">
-            <LogoApp />
-            <div className="header__formWrapper">
-              <HeaderForm />
-            </div>
-
-            <button className="header__btnIcon">
-              <i className="fa-solid fa-cart-shopping"></i>Koszyk
-            </button>
+    <header className="header">
+      <HeaderWidget />
+      <div className="header__serch">
+        <div className="container header__container">
+          <LogoApp />
+          <div className="header__formWrapper">
+            <HeaderForm />
           </div>
+
+          <button className="header__btnIcon">
+            <i className="fa-solid fa-cart-shopping"></i>Koszyk
+          </button>
         </div>
-        <HeaderMenu />
-      </header>
-      <Outlet />
-    </>
+      </div>
+      <HeaderMenu />
+    </header>
   );
 }
