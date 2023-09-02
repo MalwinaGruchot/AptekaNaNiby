ffa377f4e6c95270083c289b43aa49ae5ace790b
MalwinaGruchot <malwinagruchot@wp.pl>
2023-09-03 00:32:40 +0200

    make home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
 import SectionBanner from "./SectionBanner";
+import SectionForYou from "./SectionForYou";
 
 export default function Home() {
   return (
     <>
       <SectionBanner />
+      <SectionForYou />
     </>
   );
 }
