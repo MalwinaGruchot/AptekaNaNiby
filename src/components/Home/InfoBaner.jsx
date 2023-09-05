ab2db83cffb81106f3fffb40b37a1045b22ee593
MalwinaGruchot <malwinagruchot@wp.pl>
2023-09-05 17:51:54 +0200

    fix commit

--- src/components/Home/InfoBaner.jsx
+++ src/components/Home/InfoBaner.jsx
@@ -3,10 +3,13 @@ import styles from "./InfoBaner.module.scss";
 // eslint-disable-next-line react/prop-types
 export default function InfoBaner({ src }) {
   return (
-    <div className={styles.infoBaner}>
-      <div className={styles.wrapper}>
-        <img src={src}></img>
-        <h3 className={styles.title}>Wybrane dla Ciebie</h3>
+    <div className={styles.baner} style={{ backgroundImage: `url(${src})` }}>
+      <div className={styles.background}>
+        <div className={styles.titleWrapper}>
+          <h3 className={styles.titleBaner}> Wybrane </h3>
+          <h3 className={styles.titleBaner}> dla </h3>
+          <h3 className={styles.titleBaner}>Ciebie </h3>
+        </div>
       </div>
     </div>
   );
