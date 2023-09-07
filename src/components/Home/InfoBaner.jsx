8fb75de60a6d6d6898b1d57cb80085b50323281e
MalwinaGruchot <malwinagruchot@wp.pl>
2023-09-07 18:53:12 +0200

    make infobaner

--- src/components/Home/InfoBaner.jsx
+++ src/components/Home/InfoBaner.jsx
@@ -1,14 +1,14 @@
 import styles from "./InfoBaner.module.scss";
 
 // eslint-disable-next-line react/prop-types
-export default function InfoBaner({ src }) {
+export default function InfoBaner({ src, title, backgroundColor }) {
   return (
     <div className={styles.baner} style={{ backgroundImage: `url(${src})` }}>
-      <div className={styles.background}>
+      <div className={styles.background} style={{ backgroundColor }}>
         <div className={styles.titleWrapper}>
-          <h3 className={styles.titleBaner}> Wybrane </h3>
-          <h3 className={styles.titleBaner}> dla </h3>
-          <h3 className={styles.titleBaner}>Ciebie </h3>
+          <h3 className={styles.titleBaner}> {title[0]} </h3>
+          <h3 className={styles.titleBaner}> {title[1]} </h3>
+          <h3 className={styles.titleBaner}>{title[2]} </h3>
         </div>
       </div>
     </div>
