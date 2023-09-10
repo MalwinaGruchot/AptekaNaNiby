a31a7b64d5422bf12d6bef7f5d574d35d1837c31
MalwinaGruchot <malwinagruchot@wp.pl>
2023-09-10 23:18:40 +0200

    make App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RouteWrapper from "./RouteWrapper/RouteWrapper";
 import Home from "./Home/Home";
 import ProductCard from "./ProductCard/ProductCard";
+import PageCategory from "./PageCategory/PageCategory";
+import Article from "./Article/Article";
 
 function App() {
   return (
@@ -11,11 +13,23 @@ function App() {
         <Route path="/" element={<RouteWrapper />}>
           <Route path="/" element={<Home />} />
           <Route path="/products/:id" element={<ProductCard />} />
-          <Route path="/article/:id" element={<p>artykuł</p>} />
-          <Route path="/pielęgnacja" element={<div>pielęgnacja</div>} />
-          <Route path="/srodkiopatrunkowe" element={<div>opatrunkowe</div>} />
-          <Route path="/witaminy" element={<div>witaminy</div>} />
-          <Route path="/sprzętmedyczny" element={<div>Sprzet</div>} />
+          <Route path="/article/:id" element={<Article />} />
+          <Route
+            path="/promocje"
+            element={<PageCategory category={"Promocje"} />}
+          />
+          <Route
+            path="/pielęgnacja"
+            element={<PageCategory category={"Pielęgnacja"} />}
+          />
+          <Route
+            path="/srodkiopatrunkowe"
+            element={<PageCategory category={"Środki opatrunkowe"} />}
+          />
+          <Route
+            path="/sprzętmedyczny"
+            element={<PageCategory category={"Sprzęt medyczny"} />}
+          />
         </Route>
       </Routes>
     </BrowserRouter>
