f8c6ae51452141abcece0b862e7f25c566da39ac
MalwinaGruchot <malwinagruchot@wp.pl>
2023-09-10 23:17:16 +0200

    Pagecategory i data

---
+++ src/components/PageCategory/PageCategory.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import styles from "./PageCategory.module.scss";
+import { getProdukts } from "../../api/getProdukts";
+import ProductLink from "../ProductLink/ProductLink";
+import { NavLink } from "react-router-dom";
+
+export default function PageCategory({ category }) {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProdukts(category, setProducts);
+  }, []);
+
+  if (!products.length) {
+    return <p>Wczytywanie...</p>;
+  }
+
+  return (
+    <div className={styles.section}>
+      <div className={styles.container}>
+        <h3 className={styles.title}>{category}</h3>
+        <div className={styles.wrapper}>
+          {products.map((product) => (
+            <NavLink
+              className={styles.navLink}
+              key={product.id}
+              to={`/pielęgnacja/${product.id}`}
+            >
+              <ProductLink product={product} />
+            </NavLink>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
