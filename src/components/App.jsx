7da1c99d960a8fbf8f9af4c205aae188bc51f239
MalwinaGruchot <malwinagruchot@wp.pl>
2023-09-11 23:19:09 +0200

    make AppProvider

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,34 +5,37 @@ import Home from "./Home/Home";
 import ProductCard from "./ProductCard/ProductCard";
 import PageCategory from "./PageCategory/PageCategory";
 import Article from "./Article/Article";
+import AppProvider from "../providers/AppProvider";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<RouteWrapper />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/products/:id" element={<ProductCard />} />
-          <Route path="/article/:id" element={<Article />} />
-          <Route
-            path="/promocje"
-            element={<PageCategory category={"Promocje"} />}
-          />
-          <Route
-            path="/pielęgnacja"
-            element={<PageCategory category={"Pielęgnacja"} />}
-          />
-          <Route
-            path="/srodkiopatrunkowe"
-            element={<PageCategory category={"Środki opatrunkowe"} />}
-          />
-          <Route
-            path="/sprzętmedyczny"
-            element={<PageCategory category={"Sprzęt medyczny"} />}
-          />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <AppProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<RouteWrapper />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/products/:id" element={<ProductCard />} />
+            <Route path="/article/:id" element={<Article />} />
+            <Route
+              path="/promocje"
+              element={<PageCategory category={"Promocje"} />}
+            />
+            <Route
+              path="/pielęgnacja"
+              element={<PageCategory category={"Pielęgnacja"} />}
+            />
+            <Route
+              path="/srodkiopatrunkowe"
+              element={<PageCategory category={"Środki opatrunkowe"} />}
+            />
+            <Route
+              path="/sprzętmedyczny"
+              element={<PageCategory category={"Sprzęt medyczny"} />}
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AppProvider>
   );
 }
 
---
+++ src/providers/AppProvider.jsx
@@ -0,0 +1,13 @@
+import { useState, createContext } from "react";
+
+export const AppContext = createContext();
+
+export default function AppProvider({ children }) {
+  const [basket, setBasket] = useState([]);
+
+  return (
+    <AppContext.Provider value={{ basket, setBasket }}>
+      {children}
+    </AppContext.Provider>
+  );
+}
