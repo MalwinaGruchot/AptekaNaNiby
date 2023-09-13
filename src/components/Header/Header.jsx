8fc2dc2503115533b321bbc1e823fb5d39e107da
MalwinaGruchot <malwinagruchot@wp.pl>
2023-09-13 23:36:26 +0200

    make style form basket

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,8 +4,11 @@ import HeaderWidget from "./HeaderWidget";
 import HeaderForm from "./HeaderForm";
 import HeaderMenu from "./HeaderMenu";
 import { NavLink } from "react-router-dom";
+import { useContext } from "react";
+import { AppContext } from "../../providers/AppProvider";
 
 export default function Header() {
+  const { basket } = useContext(AppContext);
   return (
     <>
       <HeaderWidget />
@@ -18,6 +21,9 @@ export default function Header() {
             </div>
 
             <NavLink to="/basket" className="header__btnIcon">
+              {basket.length > 0 ? (
+                <div className="header__number">{basket.length}</div>
+              ) : null}
               <i className="fa-solid fa-cart-shopping"></i>Koszyk
             </NavLink>
           </div>
--- src/components/PageBasket/PageBasket.jsx
+++ src/components/PageBasket/PageBasket.jsx
@@ -6,6 +6,17 @@ import { NavLink } from "react-router-dom";
 
 export default function PageBasket() {
   const { basket } = useContext(AppContext);
+
+  if (basket.length === 0) {
+    return (
+      <section className={style.basket}>
+        <div className={style.container}>
+          <h2 className={style.title}>Twój koszyk na niby jest pusty.</h2>
+        </div>
+      </section>
+    );
+  }
+
   return (
     <section className={style.basket}>
       <div className={style.container}>
@@ -22,6 +33,10 @@ export default function PageBasket() {
               </NavLink>
             ))}
           </div>
+          <section className={style.price}>
+            <h3>Suma:</h3>
+            <h3>50,00zł</h3>
+          </section>
         </div>
       </div>
     </section>
