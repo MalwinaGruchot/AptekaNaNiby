07c0290d1eead1ca74e04509b38bedfe49151082
MalwinaGruchot <malwinagruchot@wp.pl>
2023-09-16 22:14:51 +0200

    make mobile styles

--- src/components/Header/HeaderForm.jsx
+++ src/components/Header/HeaderForm.jsx
@@ -1,24 +1,33 @@
-import { NavLink } from "react-router-dom";
-import "./HeaderForm.scss";
+import { useNavigate } from "react-router-dom";
+import styles from "./HeaderForm.module.scss";
 import { useContext } from "react";
 import { AppContext } from "../../providers/AppProvider";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 export default function HeaderForm() {
   const { param, setParam } = useContext(AppContext);
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    navigate("/wyszukaj");
+  };
 
   return (
-    <form className="header__form">
+    <form className={styles.form} onSubmit={handleSubmit}>
       <input
         name="serch"
         type="text"
-        className="header__input"
+        className={styles.input}
         placeholder="Wyszukaj..."
         value={param}
         onChange={(e) => setParam(e.target.value)}
       ></input>
-      <NavLink to="/wyszukaj">
-        <i className="fa-solid fa-magnifying-glass"></i>
-      </NavLink>
+      <button className={styles.button} type="submit">
+        <FontAwesomeIcon className={styles.fasolid} icon={faMagnifyingGlass} />
+      </button>
     </form>
   );
 }
--- src/components/Header/HeaderMenu.jsx
+++ src/components/Header/HeaderMenu.jsx
@@ -1,5 +1,9 @@
 import { NavLink } from "react-router-dom";
 import styles from "./HeaderMenu.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import HeaderForm from "./HeaderForm";
+import { useState } from "react";
 
 const links = [
   { title: "Promocje", url: "promocje" },
@@ -9,28 +13,82 @@ const links = [
 ];
 
 export default function HeaderMenu() {
+  const [isActive, setIsActive] = useState(true);
+
+  const handleClick = () => {
+    setIsActive((prev) => !prev);
+  };
+
   return (
-    <div className={styles.menu}>
-      <div className={styles.container}>
-        <nav className={styles.nav}>
-          <ul className={styles.navList}>
-            {links.map(({ title, url }) => (
-              <li className={styles.item} key={url}>
-                <NavLink
-                  className={({ isActive }) =>
-                    isActive
-                      ? `${styles.navLink} ${styles.active}`
-                      : `${styles.navLink}`
-                  }
-                  to={`/${url}`}
-                >
-                  {title}
-                </NavLink>
-              </li>
-            ))}
-          </ul>
-        </nav>
+    <>
+      <div className={styles.mobileBar}>
+        <div className={styles.container}>
+          <div className={styles.mobile}>
+            <div className={styles.wrapperform}>
+              <HeaderForm />
+            </div>
+            {isActive ? (
+              <FontAwesomeIcon
+                onClick={handleClick}
+                className={styles.fabar}
+                icon={faBars}
+              />
+            ) : (
+              <FontAwesomeIcon
+                onClick={handleClick}
+                className={styles.fabar}
+                icon={faXmark}
+              />
+            )}
+          </div>
+        </div>
       </div>
-    </div>
+      {isActive ? null : (
+        <div className={styles.menuMobile}>
+          <div className={styles.container}>
+            <nav className={styles.nav}>
+              <ul className={styles.navList}>
+                {links.map(({ title, url }) => (
+                  <li className={styles.item} key={url}>
+                    <NavLink
+                      className={({ isActive }) =>
+                        isActive
+                          ? `${styles.navLink} ${styles.active}`
+                          : `${styles.navLink}`
+                      }
+                      to={`/${url}`}
+                    >
+                      {title}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </div>
+        </div>
+      )}
+      <div className={styles.menu}>
+        <div className={styles.container}>
+          <nav className={styles.nav}>
+            <ul className={styles.navList}>
+              {links.map(({ title, url }) => (
+                <li className={styles.item} key={url}>
+                  <NavLink
+                    className={({ isActive }) =>
+                      isActive
+                        ? `${styles.navLink} ${styles.active}`
+                        : `${styles.navLink}`
+                    }
+                    to={`/${url}`}
+                  >
+                    {title}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </>
   );
 }
