8b7126e6c60c412a6e81c20b46ef051d45a04a22
MalwinaGruchot <malwinagruchot@wp.pl>
2023-09-23 11:04:20 +0200

    img article in array

--- src/components/ArticleLink/ArticleLink.jsx
+++ src/components/ArticleLink/ArticleLink.jsx
@@ -1,9 +1,15 @@
 import styles from "./ArticleLink.module.scss";
-import imgt from "../../assets/witd2.jpg";
+import img1 from "../../assets/witd2.jpg";
+import img2 from "../../assets/kremart.jpg";
+import img3 from "../../assets/smoothie.jpg";
+const array = [img1, img2, img3];
 
 export default function ArticleLink({ src, title }) {
   return (
-    <div className={styles.baner} style={{ backgroundImage: `url(${imgt})` }}>
+    <div
+      className={styles.baner}
+      style={{ backgroundImage: `url(${array[src]})` }}
+    >
       <div className={styles.background}>
         <div className={styles.titleWrapper}>
           <h3 className={styles.titleBaner}> {title} </h3>
