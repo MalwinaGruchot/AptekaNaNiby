4f7192c2a2b1d7839926dc606ab100db5316f05a
MalwinaGruchot <malwinagruchot@wp.pl>
2023-09-23 11:14:06 +0200

    article img

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -3,6 +3,10 @@ import { getArticle } from "../../api/getArticle";
 import { useParams } from "react-router-dom";
 import styles from "./Article.module.scss";
 import { constArticle } from "../../assets/const";
+import img2 from "../../assets/witd2.jpg";
+import img0 from "../../assets/kremart.jpg";
+import img1 from "../../assets/smoothie.jpg";
+const array = [img0, img1, img2];
 
 export default function Article() {
   const { id } = useParams();
@@ -20,7 +24,7 @@ export default function Article() {
     <article className={styles.article}>
       <div className={styles.container}>
         <div className={styles.imgWrapp}>
-          <img className={styles.img} src={article.url}></img>
+          <img className={styles.img} src={array[article.url]}></img>
         </div>
         <h2 className={styles.title}>{article.name}</h2>
         <p className={styles.text}>{constArticle[0]}</p>
