c6e6d31f949e659c49706c8b74b13e05a102a2f0
MalwinaGruchot <malwinagruchot@wp.pl>
2023-09-23 10:34:18 +0200

    articles img

--- src/components/ArticleLink/ArticleLink.jsx
+++ src/components/ArticleLink/ArticleLink.jsx
@@ -1,11 +1,9 @@
 import styles from "./ArticleLink.module.scss";
+import imgt from "../../assets/witd2.jpg";
 
 export default function ArticleLink({ src, title }) {
   return (
-    <div
-      className={styles.baner}
-      style={{ backgroundImage: `url("../../src/assets/witd2.jpg")` }}
-    >
+    <div className={styles.baner} style={{ backgroundImage: `url(${imgt})` }}>
       <div className={styles.background}>
         <div className={styles.titleWrapper}>
           <h3 className={styles.titleBaner}> {title} </h3>
